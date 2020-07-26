ca35cd608af43dcd729ab875827cef2ea72c1516
yokoiakinori <yokoiakinori@icloud.com>
2020-07-26 23:07:06 +0900

    修正

--- store/index.js
+++ store/index.js
@@ -1,25 +1,25 @@
-// export const state = () => ({
-//   latestNews: [],
-// })
+export const state = () => ({
+  latestNews: [],
+})
 
-// export const mutations = {
-//   setlatestNews(state, list) {
-//     state.latestNews = list
-//   },
-// }
+export const mutations = {
+  setlatestNews(state, list) {
+    state.latestNews = list
+  },
+}
 
-// export const actions = {
-//   async nuxtServerInit({ commit }) {
-//     const files = await require.context(
-//       '~/assets/content/LatestNews',
-//       false,
-//       /\.json$/
-//     )
-//     const newsPosts = files.keys().map((key) => {
-//       const res = files(key)
-//       res.slug = key.slice(2, -5)
-//       return res
-//     })
-//     await commit('setlatestNews', newsPosts)
-//   },
-// }
+export const actions = {
+  async nuxtServerInit({ commit }) {
+    const files = await require.context(
+      '~/assets/content/LatestNews',
+      false,
+      /\.json$/
+    )
+    const newsPosts = files.keys().map((key) => {
+      const res = files(key)
+      res.slug = key.slice(2, -5)
+      return res
+    })
+    await commit('setlatestNews', newsPosts)
+  },
+}
